92793ec6d228f19602958e30fa00fcf41fa88ec8
Mrt0t0 <47758913+Mrt0t0@users.noreply.github.com>
2026-04-13 17:52:42 +0200

    Update load function to use optional port parameter
    Modify load function to accept an optional port parameter and remove port replacement in HTML.

--- minitel-ha/pagehtml.py
+++ minitel-ha/pagehtml.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 _html = ""
 
-def load(port):
+def load(port=None):
     global _html
     p = Path(__file__).parent / 'static' / 'index.html'
-    _html = p.read_text(encoding='utf-8').replace('__WB_PORT__', str(port))
+    _html = p.read_text(encoding='utf-8')
 
 def get():
     return _html
